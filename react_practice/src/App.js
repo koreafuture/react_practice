70285b771432fa5a99816dd3c5caf5dd6873fa65
koreafuture <41562375+koreafuture@users.noreply.github.com>
2023-01-11 21:20:07 -0800

    component and props

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,24 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+function Header(props){
+  return <header>
+  <h1><a href="/">{props.title}</a></h1>
+</header>
+}
+function Article(props){
+  return <article>
+  <h2>{props.title}</h2>
+  {props.body}
+</article>
+}
+function Nav(props){
+  const lis = [
+   
+  ]
+  for(let i =0; i<props.topics.length;i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>,
+    <li key={t.id2}>{t.body}</li>)
+  }
+  return <nav>
+  <ol>
+    {lis}
+  </ol>
+</nav>
+}
 
 function App() {
+  const topics = [
+    {id:1,id2:4, title:'html', body:'htlm is ....'},
+    {id:2,id2:5, title:'css', body:'css is ....'},
+    {id:3,id2:6, title:'js', body:'js is ....'}
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="REACT"></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="WECOME" body="BODYDY"></Article>
     </div>
+    
   );
 }
 
